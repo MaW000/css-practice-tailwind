9592e58bf61ff1aafee9aed4b7563d2ec90b4340
matt wardlow <mwardlow0@gmail.com>
2022-11-17 23:55:38 -0500

    promo-banner

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      paleYellow: '#c5ab5f',
+      goldText: '#645a24'
+
+    },
+    extend: {
+
+    },
   },
   plugins: [],
 };
